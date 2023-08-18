9cb9eb77a71f1d7b1d6fd30837a49a8b1635ea0d
aydosjs <aji3385@gmail.com>
2023-08-18 14:28:12 +0500

    book list and edit is done

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,8 +1,7 @@
-import { APIResponse, User } from "../types/common";
+import { User } from "../types/common";
 import { post } from "./ApiClient";
 
 export async function register(data: Omit<User, "id">) {
-  const res = await post<APIResponse<User>>("/signup", data);
-
-  return res.data;
+  const res = await post<User>("/signup", data);
+  return res;
 }
