67ea0e947c5d057ea5b98eea018fb7090384603e
aydosjs <aji3385@gmail.com>
2023-08-18 21:42:02 +0500

    containter

--- src/containers/auth/RegisterContainer.tsx
+++ src/containers/auth/RegisterContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Typography } from "@mui/material"
+import { Box, Button, Container, TextField, Typography } from "@mui/material"
 import { useFormik } from 'formik';
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
@@ -28,89 +28,90 @@ export default function RegisterContainer() {
   });
 
   return (
-    <Box sx={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "26%"
-        }}
-      >
-        <Box mb={4} width={'100%'}>
-          <Typography component="h1" variant="h4" sx={{ fontWeight: "500", textAlign: "start" }}>
-            Create an account
-          </Typography>
-          <Typography component="p" variant="body1" color="textSecondary">
-            Welcome to BookShelf
-            {/* If you already have an account: <Typography component={Link} color='blue' to={'/login'}>Login</Typography> */}
-          </Typography>
+    <Container maxWidth="sm">
+      <Box sx={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        <Box
+          sx={{
+            width: "68%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box mb={4} width={'100%'}>
+            <Typography component="h1" variant="h4" sx={{ fontWeight: "500", textAlign: "start" }}>
+              Create an account
+            </Typography>
+            <Typography component="p" variant="body1" color="textSecondary">
+              Welcome to BookShelf
+              {/* If you already have an account: <Typography component={Link} color='blue' to={'/login'}>Login</Typography> */}
+            </Typography>
+          </Box>
+
+          <Box component="form" onSubmit={formik.handleSubmit} sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+
+            <TextField
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+              error={Boolean(formik.touched.name && formik.errors.name)}
+            />
+
+            <Box mt={2}>
+              <TextField
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                error={Boolean(formik.touched.email && formik.errors.email)}
+              />
+            </Box>
+
+            <Box mt={2}>
+              <TextField
+                fullWidth
+                name="key"
+                label="Key"
+                id="key"
+                onChange={formik.handleChange}
+                value={formik.values.key}
+                error={Boolean(formik.touched.key && formik.errors.key)}
+              />
+            </Box>
+
+            <Box mt={2}>
+              <TextField
+                fullWidth
+                name="secret"
+                label="Secret"
+                id="secret"
+                onChange={formik.handleChange}
+                value={formik.values.secret}
+                error={Boolean(formik.touched.secret && formik.errors.secret)}
+              />
+            </Box>
+
+            <Button
+              type="submit"
+              fullWidth
+              size="large"
+              variant="contained"
+              sx={{ mt: 4 }}
+            >
+              Sign In
+            </Button>
+
+          </Box>
+
         </Box>
-
-        <Box component="form" onSubmit={formik.handleSubmit} sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
-
-          <TextField
-            fullWidth
-            id="name"
-            label="Name"
-            name="name"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-            error={Boolean(formik.touched.name && formik.errors.name)}
-          />
-
-          <Box mt={2}>
-            <TextField
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              onChange={formik.handleChange}
-              value={formik.values.email}
-              error={Boolean(formik.touched.email && formik.errors.email)}
-            />
-          </Box>
-
-          <Box mt={2}>
-            <TextField
-              fullWidth
-              name="key"
-              label="Key"
-              id="key"
-              onChange={formik.handleChange}
-              value={formik.values.key}
-              error={Boolean(formik.touched.key && formik.errors.key)}
-            />
-          </Box>
-
-          <Box mt={2}>
-            <TextField
-              fullWidth
-              name="secret"
-              label="Secret"
-              id="secret"
-              onChange={formik.handleChange}
-              value={formik.values.secret}
-              error={Boolean(formik.touched.secret && formik.errors.secret)}
-            />
-          </Box>
-
-          <Button
-            type="submit"
-            fullWidth
-            size="large"
-            variant="contained"
-            sx={{ mt: 4 }}
-          >
-            Sign In
-          </Button>
-
-        </Box>
-
-      </Box>
-    </Box >
+      </Box >
+    </Container>
   )
 }
--- src/containers/layout/MainLayout.tsx
+++ src/containers/layout/MainLayout.tsx
@@ -1,18 +1,20 @@
-import { Box, CssBaseline } from "@mui/material";
+import { Box, Container, CssBaseline } from "@mui/material";
 import Sidebar from "./Sidebar";
 
 export default function MainLayout({ children }: React.PropsWithChildren) {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Sidebar />
+    <Container maxWidth="xl">
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Sidebar />
 
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-      >
-        {children}
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+        >
+          {children}
+        </Box>
       </Box>
-    </Box>
+    </Container>
   )
 }
